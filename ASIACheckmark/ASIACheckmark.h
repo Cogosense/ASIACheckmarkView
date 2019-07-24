1e681daf4160459e0720f9478d7e48dff2e5dfbe
Steve Williams <steve.williams@telxio.com>
2019-07-24 16:34:30 -0700

    Update to Swift5 Using Xcode10.3
    Migrated from Swift4 to Swift5

---
+++ ASIACheckmark/ASIACheckmark.h
@@ -0,0 +1,19 @@
+//
+//  ASIACheckmark.h
+//  ASIACheckmark
+//
+//  Created by Steve Williams on 2019-07-24.
+//  Copyright © 2019 Andrzej Michnia Usługi Programistyczne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ASIACheckmark.
+FOUNDATION_EXPORT double ASIACheckmarkVersionNumber;
+
+//! Project version string for ASIACheckmark.
+FOUNDATION_EXPORT const unsigned char ASIACheckmarkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASIACheckmark/PublicHeader.h>
+
+
